042f7f12c5237fa56c7e397337e49b6b9e3da50c
Kaya Mangtay <kmangtay@lcs.on.ca>
2026-02-27 09:38:55 -0500

    Initial Commit

---
+++ Clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Seyhan Kaya Mangtay on 2026-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
